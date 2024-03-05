662a5a197b1dde6bbd36de225a63b57398f64d54
tejaksh <0102lucifer@gmail.com>
2024-03-06 00:36:11 +0530

    PAGINATION AND SORTING FEATURE

--- src/main/java/com/codedecode/demo/controller/EmployeeController.java
+++ src/main/java/com/codedecode/demo/controller/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.codedecode.demo.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.codedecode.demo.entity.Employee;
 import com.codedecode.demo.service.EmployeeServiceInterface;
+import utils.AppConstants;
 
 @RestController
 @RequestMapping("/code")
@@ -31,7 +31,11 @@ public class EmployeeController {
 	}
 	
 	@GetMapping("/all")
-	public ResponseEntity<List<Employee>> getAllEmployees(){
+	public ResponseEntity<List<Employee>> getAllEmployees(
+			@RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir){
 		
 		List<Employee> listOfAllEmps = employeeServiceInterface.getAllEmployees();
 		return new ResponseEntity<List<Employee>>(listOfAllEmps, HttpStatus.OK);
