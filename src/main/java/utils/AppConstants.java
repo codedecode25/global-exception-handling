bc1b73e4f5ab5af56af483138125f66d937b562e
tejaksh <0102lucifer@gmail.com>
2024-03-06 00:43:24 +0530

    APP CONSTANST ADDED

---
+++ src/main/java/utils/AppConstants.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class AppConstants {
+	 public static final String DEFAULT_PAGE_NUMBER = "0";
+	    public  static final String DEFAULT_PAGE_SIZE = "10";
+	    public static final String DEFAULT_SORT_BY = "id";
+	    public static final String DEFAULT_SORT_DIRECTION = "asc";
+
+}
